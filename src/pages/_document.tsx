13eadd225d7fa32bfb2b3a43971c4a3d94c3ca84
Anderson Santos <Anderson_Santos__@outlook.com>
2021-06-08 01:37:23 -0300

    home static props + document title bugfix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,10 +42,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
           <meta property="og:image" content="/assets/image/preview.png" />
         </Head>
         <body>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,98 +1,18 @@
-import { useEffect, useState } from 'react'
 import { HomeContainer, HomeContentContainer } from '../styles/pages/home'
-import axios from 'axios'
 import GamesLibrarySection from '../components/NewGamesLibrarySection'
 import Head from 'next/head'
 import RatedGamesGrid from '../components/RatedGamesGrid'
 import SearchHeader from '../components/SearchHeader'
-import UserService from '../services/users'
-import AuthService from '../services/auth'
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
 
-const Home = () => {
-  const [topGames, setTopGames] = useState([])
-  const [ratedGames, setRatedGames] = useState([])
-  const [newestGames, setNewestGames] = useState([])
-  const [comingGames, setComingGames] = useState([])
-  const [recentlyReviewedGames, setRecentlyReviewedGames] = useState([])
-  const [loadingTopGames, setLoadingTopGames] = useState(true)
-  const [loadingRatedGames, setLoadingRatedGames] = useState(true)
-  const [loadingNewestGames, setLoadingNewestGames] = useState(true)
-  const [loadingComingGames, setLoadingComingGames] = useState(true)
-  const [loadingRecentlyReviewedGames, setLoadingRecentlyReviewedGames] =
-    useState(true)
-
-  const [logged, setLogged] = useState<Promise<boolean> | boolean>(
-    AuthService.isLoggedIn
-  )
-
-  AuthService.observeStatus(user => setLogged(!!user))
-
-  const params = {
-    qtd: 30,
-    index: 0
-  }
-
-  const getRatedGames = async () => {
-    setLoadingRatedGames(true)
-    const response = await axios.get('/api/games/rating', {
-      params
-    })
-
-    setRatedGames(response.data)
-    setLoadingRatedGames(false)
-  }
-
-  const getTopGames = async () => {
-    setLoadingTopGames(true)
-    const response = await axios.get('/api/games/top', {
-      params
-    })
-
-    setTopGames(response.data)
-    setLoadingTopGames(false)
-  }
-
-  const getNewestGames = async () => {
-    setLoadingNewestGames(true)
-    const response = await axios.get('/api/games/newest', {
-      params
-    })
-
-    setNewestGames(response.data)
-    setLoadingNewestGames(false)
-  }
-
-  const getComingGames = async () => {
-    setLoadingComingGames(true)
-    const response = await axios.get('/api/games/coming', {
-      params
-    })
-
-    setComingGames(response.data)
-    setLoadingComingGames(false)
-  }
-
-  const getRecentlyReviwedGames = async () => {
-    setLoadingRecentlyReviewedGames(true)
-    const response = await axios.get('/api/games/recently-reviewed', {
-      params: {
-        qtd: 5,
-        index: 0
-      }
-    })
-
-    setRecentlyReviewedGames(response.data)
-    setLoadingRecentlyReviewedGames(false)
-  }
-
-  useEffect(() => {
-    getTopGames()
-    getRatedGames()
-    getNewestGames()
-    getComingGames()
-    getRecentlyReviwedGames()
-  }, [])
-
+const Home = ({
+  ratedGames,
+  reviewedGames,
+  topGames,
+  newestGames,
+  comingGames
+}) => {
   /* useEffect(() => {
     if (logged) {
       UserService.getUsers()
@@ -112,32 +32,25 @@ const Home = () => {
       <SearchHeader />
       <HomeContentContainer>
         <GamesLibrarySection
-          loading={loadingRatedGames}
           title={`Populares de ${new Date().getFullYear()}`}
           type={'slide'}
           data={ratedGames}
           showRating
         />
         <h1>Revisados recentemente</h1>
-        <RatedGamesGrid
-          games={recentlyReviewedGames}
-          loading={loadingRecentlyReviewedGames}
-        />
+        <RatedGamesGrid games={reviewedGames} />
         <GamesLibrarySection
-          loading={loadingTopGames}
           title="Populares"
           type={'slide'}
           data={topGames}
           showRating
         />
         <GamesLibrarySection
-          loading={loadingNewestGames}
           title="Novidades"
           type={'slide'}
           data={newestGames}
         />
         <GamesLibrarySection
-          loading={loadingComingGames}
           title="Próximos Lançamentos"
           type={'slide'}
           data={comingGames}
@@ -145,6 +58,44 @@ const Home = () => {
       </HomeContentContainer>
     </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const params = { qtd: 30 }
+
+  const getGames = async () => {
+    const ratedGames = api.grapi.get('/games/rating', {
+      params
+    })
+    const topGames = api.grapi.get('/games/top', {
+      params
+    })
+    const newestGames = api.grapi.get('/games/newest', {
+      params
+    })
+    const comingGames = api.grapi.get('/games/coming', {
+      params
+    })
+    const reviewedGames = api.grapi.get('/games/recently-reviewed', {
+      params: { qtd: 5 }
+    })
+
+    return {
+      ratedGames: (await ratedGames).data,
+      topGames: (await topGames).data,
+      newestGames: (await newestGames).data,
+      comingGames: (await comingGames).data,
+      reviewedGames: (await reviewedGames).data
+    }
+  }
+
+  const { ratedGames, topGames, newestGames, comingGames, reviewedGames } =
+    await getGames()
+
+  return {
+    props: { ratedGames, topGames, newestGames, comingGames, reviewedGames },
+    revalidate: 60 * 10 // 10 minutes
+  }
 }
 
 export default Home
